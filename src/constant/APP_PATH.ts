cbad6a43116e04f9a566b3400c3ade00a5b66d03
Son-Huynh <huynhphuson17@gmail.com>
2022-10-12 09:43:55 +0700

    add more path

--- src/constant/APP_PATH.ts
+++ src/constant/APP_PATH.ts
@@ -1,5 +1,8 @@
 const APP_PATH = {
-    ROOT: '/',
-    PRODUCTS: '/products',
+  ROOT: "/",
+  PRODUCTS: "/products",
+  SIGNIN: "/signin",
+  SIGNUP: "/signup",
+  FORGOTPASSWORD: "/forgot",
 };
 export default APP_PATH;
