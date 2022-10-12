a8dcac21dad37feee6c0086af688302c3ec3ca87
Son-Huynh <huynhphuson17@gmail.com>
2022-10-12 09:44:10 +0700

    fix import

--- src/pages/Homepage/Homepage.tsx
+++ src/pages/Homepage/Homepage.tsx
@@ -1,12 +1,8 @@
-import React from 'react'
-import Navbar from '../../components/Navbar'
+import React from "react";
+import { Navbar } from "../../components";
 
 const Homepage = () => {
-  return (
-    <div>
-      Homepage
-    </div>
-  )
-}
+  return <div>Homepage</div>;
+};
 
-export default Homepage
+export default Homepage;
